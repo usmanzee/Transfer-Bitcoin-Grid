ffd50e9dc3e184c222adc518556514047f373aff
usmanzee <usman.jamil0308@gmail.com>
2018-09-18 13:14:07 +0500

    redirect urls change

--- resources/views/transaction-requests.blade.php
+++ resources/views/transaction-requests.blade.php
@@ -30,7 +30,7 @@
                     <td>{{ $transactionRequest->amountInBTC }}</td>
                     <td>{{ $transactionRequest->amountInUSD }}</td>
                     <td id="action_link_{{ $transactionRequest->id }}">@if($transactionRequest->status) Paid @else <a href="" class="payment_button" requestId="{{ $transactionRequest->id }}" amountInBTC="{{ $transactionRequest->amountInBTC }}" bitcoinAccountAddress="{{ $transactionRequest->bitcoin_account_address }}">Send payment</a> @endif</td>
-                    <td>{{ date('d M, Y h:i A', strtotime($transactionRequest->created_at)) }}</td>
+                    <td>{{ date('d M, Y - h:i A', strtotime($transactionRequest->created_at)) }}</td>
                 </tr>
                 @endforeach
             </tbody>
@@ -50,6 +50,11 @@
         </table>
     </div>
 </div>
+<div id="loading_div" hidden style="margin: 0px; padding: 0px; position: fixed; right: 0px; top: 0px; width: 100%; height: 100%; background-color: rgb(102, 102, 102); z-index: 30001; opacity: 0.8;">
+    <p style="position: absolute; color: White; top: 50%; left: 45%;">
+        Transferring funds, please wait...
+    </p>
+</div>
 @endsection
 @section('scripts')
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
@@ -62,6 +67,7 @@
 
     $(".payment_button").click(function(e) {
         e.preventDefault();
+        $("#loading_div").show();
         var requestId = $(this).attr('requestId');
         var amountInBTC = $(this).attr('amountInBTC');
         var bitcoinAccountAddress = $(this).attr('bitcoinAccountAddress');
@@ -78,6 +84,7 @@
                     bitcoinAccountAddress: bitcoinAccountAddress,
                 },
                 success: function(response) {
+                    $("#loading_div").hide();
                     if(response.status) {
                         $.toaster({ priority : 'success', title : 'Success', message : response.message });
                         $("#action_link_"+requestId).html('Paid');
